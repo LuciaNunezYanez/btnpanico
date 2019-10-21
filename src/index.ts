485c50d54dd18c405d138869b56f3e618a3b44cc
Lucia Nunez <lucia.nunez0828@gmail.com>
2019-10-21 11:42:50 -0500

    Se modificaron los headers del CORS

--- src/index.ts
+++ src/index.ts
@@ -18,7 +18,12 @@ server.app.use(bodyParser.json());
 
 // CORS - Para permitir que se puedan llamar los servicios     
 server.app.use(cors ({origin: true, credentials: true}));
-
+server.app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
+    next();
+  });
 
 // Rutas de servicios 
 server.app.use('/comercio', comercios);
