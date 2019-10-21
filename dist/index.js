3008015dc78a3ba8197b90a9a86165e9050cc6e6
Lucia Nunez <lucia.nunez0828@gmail.com>
2019-10-21 11:45:57 -0500

    Se modificaron los headers del CORS

--- dist/index.js
+++ dist/index.js
@@ -20,7 +20,7 @@ server.app.use(body_parser_1.default.json());
 // CORS - Para permitir que se puedan llamar los servicios     
 server.app.use(cors_1.default({ origin: true, credentials: true }));
 server.app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
     next();
